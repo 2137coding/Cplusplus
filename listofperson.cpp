97b62b4eb2b1be6d2e344c41578d9fa01eb2303f
2137coding <159535381+2137coding@users.noreply.github.com>
2024-04-14 10:39:07 +0200

    Update listofperson.cpp

--- listofperson.cpp
+++ listofperson.cpp
@@ -10,6 +10,7 @@ class person{
         std::string name;
         std::string surname;
         bool adult;
+        int index {0};
         int x = 15;
     
     public:
@@ -44,36 +45,95 @@ class person{
 
 class book{
     public :
-        int index;
+        int index {9999};
         std::string book_name;
         int year;
+        int x = 15;
 
-    private :
-    
+        book(){
+            std::cout << "type name of book" << std::endl;
+            std::cin >> book_name;
+            std::cout << "Type year of relase" << std::endl;
+            std::cin >> year;
+        }
+    public :
+        void display(){
+            std::cout << std::setw(x) << book_name << std::setw(x) << year << std::endl;
+        }
+        void check(){
 
+        };
+};
 
+class borrowing{
+    public :
+        int id_person {0};
+        int id_book {9999};
+        int x {15};
+
+    public :
+    void display(){
+        if(id_book != 9999){
+        std::cout << std::setw(x)  << "ID person : " << std::setw(5) << id_person << std::setw(x) << "ID book : " << std::setw(5) << id_book << std::endl;
+        }
+    }
+    void check(){
+
+    }
 
 };
+
 template <typename T>
 void add(std::vector<T>& a);
-void list(std::vector<person>& a);
+template <typename T>
+void list(std::vector<T>& a);
 void adults (std::vector<person>& a);
 void kids(std::vector<person>& a);
+template <typename T>
+void index(std::vector<T>& b);
+void get_id(std::vector<person>& a,std::vector<borrowing>& c);
+void add_id(std::vector<borrowing>& c);
 void erase(std::vector<person>& a);
 
 
 int main(){
     std::vector<person> a;
+    std::vector<book> b;
+    std::vector<borrowing> c;
     std::string command;
     while(std::cin >> command && command != "end"){
         if(command == "add"){
             add(a);
+            index(a);
         }else if(command == "list"){
             list(a);
         }else if(command == "adults"){
             adults(a);
         }else if(command == "kids"){
             kids(a);
+        }
+        if(command == "change_to_books"){
+            while(std::cin >> command && command != "change_to_persons" && command != "change_to_borrowing"){
+                if(command == "add"){
+                    add(b);
+                    index(b);
+                }else if(command == "list"){
+                    list(b);
+
+                }
+            }
+        }
+        if(command == "change_to_borrowing"){
+            while(std::cin >> command && command != "change_to_person" && command != "change_to_books"){
+            get_id(a,c);
+            if(command == "add_borrow"){
+               add_id(c);
+            }else if(command == "list"){
+                for(size_t i {0}; i < c.size();++i){
+                    c[i].display();
+                }
+            }
+        }
         }
 
     }
@@ -82,14 +142,15 @@ int main(){
 
 template <typename T>
 void add(std::vector<T>& a){
-    person newperson;
-    a.push_back(newperson);
+    T news;
+    a.push_back(news);
 }
 
-void list(std::vector<person>& a){
+template <typename T>
+void list(std::vector<T>& a){
     for(size_t i {0}; i < a.size();++i){
         a[i].check();
-        std::cout << "index : " <<  (i+1) << "  ";
+        std::cout << "index : " <<  (i) << "  ";
         a[i].display();
     }
 }
@@ -114,3 +175,33 @@ void adults(std::vector<person>& a){
         }
     }
 
+template <typename T>
+void index(std::vector<T>& b){
+    for(size_t i {0}; i < b.size();++i){
+        b[i].index = i;
+    }
+}
+
+void get_id(std::vector<person>& a, std::vector<borrowing>& c){
+    borrowing x;
+    for(size_t i {0}; i < a.size();++i){
+        c.push_back(x);
+    }
+    for(size_t i {0}; i < a.size();++i){
+        c[i].id_person = a[i].index;
+        
+    }
+}
+
+void add_id(std::vector<borrowing>& c){
+    std::cout << "Type id of person and id of book" << std::endl;
+       int i_p;
+       int i_b;
+       std::cin >> i_p >> i_b;
+       for(size_t i {0}; i < c.size();++i){
+        if(c[i].id_person == i_p){
+            c[i].id_book = i_b;
+        }
+       }
+
+}
