4d4936b1e3e0b0d8ca00d7f647349dff8b1dc1c1
2137coding <159535381+2137coding@users.noreply.github.com>
2024-04-20 11:10:49 +0200

    Update listofperson.cpp

--- listofperson.cpp
+++ listofperson.cpp
@@ -91,7 +91,7 @@ void kids(std::vector<person>& a);
 template <typename T>
 void index(std::vector<T>& b);
 void get_id(std::vector<person>& a,std::vector<borrowing>& c);
-void add_id(std::vector<borrowing>& c);
+void add_id(std::vector<borrowing>& c,std::vector<person>& a, std::vector<book>& b);
 template <typename T>
 void erase(std::vector<T>& a);
 void erase_person(std::vector<person>& a,std::vector<borrowing>& c);
@@ -105,12 +105,27 @@ int main(){
     std::vector<book> b;
     std::vector<borrowing> c;
     std::string command;
+    std::cout << "add - to add person to the list" << std::endl;
+    std::cout << "list -  to see list of persons" << std::endl;
+    std::cout << "adults - to see list of adults" << std::endl;
+    std::cout << "kids - to see list of kids" << std::endl;
+    std::cout << "erase - to erase person from the list" << std::endl;
+    std::cout << "change_to_books - to change to books" << std::endl;
+    std::cout << "change_to_borrowing - to change to borrows" << std::endl;
+    std::cout << "change_to_person - to change to persons" << std::endl;
+    std::cout << "end - to end program" << std::endl;
     while(std::cin >> command && command != "end"){
         if(command == "add"){
             add(a);
             index(a);
         }else if(command == "list"){
+            std::cout << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
             list(a);
+            std::cout << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
         }else if(command == "adults"){
             adults(a);
         }else if(command == "kids"){
@@ -119,13 +134,33 @@ int main(){
             erase_person(a,c);
             index(a);
         }
+            std::cout << "add - to add person to the list" << std::endl;
+            std::cout << "list -  to see list of persons" << std::endl;
+            std::cout << "adults - to see list of adults" << std::endl;
+            std::cout << "kids - to see list of kids" << std::endl;
+            std::cout << "erase - to erase person from the list" << std::endl;
+            std::cout << "change_to_books - to change to books" << std::endl;
+            std::cout << "change_to_borrowing - to change to borrows" << std::endl;
+            std::cout << "end - to end program" << std::endl;
         if(command == "change_to_books"){
+            std::cout << "add - to add book to the list" << std::endl;
+            std::cout << "list -  to see list of books" << std::endl;
+            std::cout << "erase - to erase book from the list" << std::endl;
+            std::cout << "change_to_person - to change to persons" << std::endl;
+            std::cout << "change_to_borrowing - to change to borrows" << std::endl;
+            std::cout << "end - to end program" << std::endl;
             while(std::cin >> command && command != "change_to_persons" && command != "change_to_borrowing"){
                 if(command == "add"){
                     add(b);
                     index(b);
                 }else if(command == "list"){
+                    std::cout << std::endl;
+                    std::cout << std::endl;
+                    std::cout << std::endl;
                     list(b);
+                    std::cout << std::endl;
+                    std::cout << std::endl;
+                    std::cout << std::endl;
 
                 }else if(command == "erase"){
                     erase(b);
@@ -134,14 +169,26 @@ int main(){
             }
         }
         if(command == "change_to_borrowing"){
+             std::cout << "add - to add borrow to the list" << std::endl;
+            std::cout << "list -  to see list of borrows" << std::endl;
+            std::cout << "erase - to erase borrow from the list" << std::endl;
+            std::cout << "change_to_books - to change to books" << std::endl;
+            std::cout << "change_to_person - to change to persons" << std::endl;
+            std::cout << "end - to end program" << std::endl;
             while(std::cin >> command && command != "change_to_person" && command != "change_to_books"){
             get_id(a,c);
             if(command == "add"){
-               add_id(c);
+               add_id(c,a,b);
             }else if(command == "list"){
+                std::cout << std::endl;
+                std::cout << std::endl;
+                std::cout << std::endl;
                 for(size_t i {0}; i < c.size();++i){
                     c[i].display();
                 }
+                std::cout << std::endl;
+                std::cout << std::endl;
+                std::cout << std::endl;
             }else if(command == "erase"){
                 erase_borrow(c);
             }
@@ -162,7 +209,7 @@ template <typename T>
 void list(std::vector<T>& a){
     for(size_t i {0}; i < a.size();++i){
         a[i].check();
-        std::cout << "index : " <<  (i) << "  ";
+        std::cout << "ID : " <<  (i) << "  ";
         a[i].display();
     }
 }
@@ -221,15 +268,23 @@ void get_id(std::vector<person>& a, std::vector<borrowing>& c){
 }
 
 
-void add_id(std::vector<borrowing>& c){
+void add_id(std::vector<borrowing>& c, std::vector<person>& a, std::vector<book>& b){
     std::cout << "Type id of person and id of book" << std::endl;
        int i_p;
        int i_b;
        std::cin >> i_p >> i_b;
+       if(i_p <= a.size() && i_b <= b.size()){
        for(size_t i {0}; i < c.size();++i){
         if(c[i].id_person == i_p){
             c[i].id_book = i_b;
         }
+       }
+       }else if(i_p > a.size() && i_b > b.size()){
+        std::cout << "two invalid ID" << std::endl;
+       }else if(i_p > a.size()){
+        std::cout << "invalid person ID" << std::endl;
+       }else if(i_b > b.size()){
+        std::cout << "invalid book ID" << std::endl;
        }
 
 }
@@ -240,7 +295,11 @@ void erase(std::vector<T>& a){
     int index1;
     std::cin >> index1;
     typename std::vector<T>::iterator x = a.begin() + index1;
+    if(index1 <= a.size()){
     a.erase(x);
+    }else{
+        std::cout << "invalid ID" << std::endl;
+    }
 }
 
 void erase_person(std::vector<person>& a,std::vector<borrowing>& c){
@@ -249,7 +308,11 @@ void erase_person(std::vector<person>& a,std::vector<borrowing>& c){
     std::cin >> indexx;
     std::vector<person>::iterator x = a.begin() + indexx;
     std::vector<borrowing>::iterator y = c.begin() + indexx;
-    a.erase(x);
+    if(indexx <= a.size()){
+       a.erase(x); 
+    }else{
+        std::cout << "invalid ID" << std::endl;
+    }
     c.erase(y);
 }
 
@@ -257,9 +320,13 @@ void erase_borrow(std::vector<borrowing>& c){
     std::cout << "type ID of person to erase the borrow" << std::endl;
     int bor;
     std::cin >> bor;
+    if(bor <= c.size()){
     for(size_t i {0}; i < c.size();++i){
         if(c[i].id_person == bor){
             c[i].id_book = 9999;
         }
+    }
+    }else{
+        std::cout << "invalid ID" << std::endl;
     }
 }
